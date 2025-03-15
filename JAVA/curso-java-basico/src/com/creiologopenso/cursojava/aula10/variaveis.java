e6b5e22f443d46b61557daad2206351e441c9fe9
lopcarv <d7m7e7@gmail.com>
2025-03-15 15:31:49 -0300

    15ma2025 15:31

--- JAVA/curso-java-basico/src/com/creiologopenso/cursojava/aula10/variaveis.java
+++ JAVA/curso-java-basico/src/com/creiologopenso/cursojava/aula10/variaveis.java
@@ -3,11 +3,12 @@ package com.creiologopenso.cursojava.aula10;
 public class variaveis {
 
 	public static void main(String[] args) {
-		int idade;
-		String nome;
-		String nomeDoMeuCachorro;
+		int idade = 20;
+		String nome = "Loiane";
+		String nomeDoMeuCachorro = "Toto";
 		
-
+		System.out.println("Idade = " + idade);
+		System.out.println("nome = " + nome);
 	}
 
 }
